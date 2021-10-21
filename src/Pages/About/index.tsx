61cbc469e045946e8a31ba18eb91719e8a1be0bb
pedro gomes <pedroleinar@hotmail.com>
2021-10-20 21:27:07 -0300

    add chakra-ui

---
+++ src/Pages/About/index.tsx
@@ -0,0 +1,9 @@
+import { Stack, Heading } from "@chakra-ui/react";
+
+export const About = () => {
+  return (
+    <Stack>
+      <Heading>About</Heading>
+    </Stack>
+  );
+};
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Stack, Heading } from "@chakra-ui/react";
+
+export const Home = () => {
+  return (
+    <Stack>
+      <Heading>Home</Heading>
+    </Stack>
+  );
+};
